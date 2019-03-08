8289a2be4019fdfa39bf087ce7da2888aa625c8e
Jose Luis Rego <regorodrijl@gmail.com>
2019-03-08 17:56:18 +0100

    Introduciendo cabecera con video y producto

--- frontoffice/js/scripts.js
+++ frontoffice/js/scripts.js
@@ -3,8 +3,9 @@ window.addEventListener("load", iniciarApp);
 
 function iniciarApp() {
     console.log('scripts.js -> iniciarApp()');
-    Moduls.header.load({url:'/animales/frontoffice/home/head.html', script:true, class:"head"});
-    Moduls.app.load({url:'/animales/frontoffice/home/home.html', script:true, class:"home"});
+    Moduls.header.load({ url: '/animales/frontoffice/home/head.html', script: true, class: "head" });
+    Moduls.app.load({ url: '/animales/frontoffice/home/home.html', script: true, class: "home" });
+    Moduls.app.children.productos.load({ url: '/animales/frontoffice/home/componentes/producto/producto.html', script: false });
 
 }
 
