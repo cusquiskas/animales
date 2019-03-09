7a59aed41297cacb9a5b0282bfa11dc55d9b46e1
CusQuisKas <cusquiskas@gmail.com>
2019-03-09 20:49:46 +0100

    ajustes en funciones

--- frontoffice/js/scripts.js
+++ frontoffice/js/scripts.js
@@ -3,7 +3,7 @@ window.addEventListener("load", iniciarApp);
 
 function iniciarApp() {
     console.log('scripts.js -> iniciarApp()');
-    Moduls.header.load({ url: '/animales/frontoffice/home/head.html', script: true});
-    Moduls.app.load({ url: '/animales/frontoffice/home/home.html', script: true});
+    Moduls.getHeader().load({ url: '/animales/frontoffice/home/head.html', script: true});
+    Moduls.getApp().load   ({ url: '/animales/frontoffice/home/home.html', script: true});
 }
 
