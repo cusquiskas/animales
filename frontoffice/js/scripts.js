527ad60912af5cc832d1ad17b17f564669b847a0
Jose Miguel <cusquiskas@gmail.com>
2020-04-19 18:38:49 +0200

    terminando de ajustar el header

--- frontoffice/js/scripts.js
+++ frontoffice/js/scripts.js
@@ -4,6 +4,6 @@ window.addEventListener("load", iniciarApp);
 function iniciarApp() {
     console.log('scripts.js -> iniciarApp()');
     Moduls.getHeader().load({ url: '/animales/frontoffice/home/head.html', script: true});
-    Moduls.getApp().load   ({ url: '/animales/frontoffice/home/home.html', script: true});
+    Moduls.getBody().load  ({ url: '/animales/frontoffice/home/home.html', script: true});
 }
 
--- index.php
+++ index.php
@@ -6,13 +6,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Animales Fantásticos y qué darles de comer</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon"       href="frontoffice/css/img/multipiensos.ico" type="image/png" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" media="screen" href="<?php echo Cache::ruta('frontoffice/css/style.css'); ?>">
 </head>
 <body>
-    <template style="border: 1px solid red" id="header"></template>
-    <template style="border: 1px solid blue" id="app"></template>
-    
-    <script src="<?php echo Cache::ruta('frontoffice/js/jquery-3.3.1.min.js'); ?>"></script>
+    <template id="header"></template>
+    <template id="body"></template>
+    <template id="footer"></template>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="frontoffice/js/jquery-3.3.1.min.js"></script>
     <script src="<?php echo Cache::ruta('frontoffice/js/funciones.js');    ?>"></script>
     <script src="<?php echo Cache::ruta('frontoffice/js/peticionAjax.js'); ?>"></script>
     <script src="<?php echo Cache::ruta('frontoffice/js/controller.js');   ?>"></script>
